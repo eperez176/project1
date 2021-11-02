d24de8a9b9fd64ae23f6063dc90fdbb230582e81
eperez176 <emanuel.perez@revature.net>
2021-11-01 23:26:13 -0500

    adding a parser fuction from json to csv

--- src/main/scala/example/test.scala
+++ src/main/scala/example/test.scala
@@ -7,25 +7,52 @@ import scala.io.StdIn.readLine;
 import java.io.IOException
 import scala.util.Try
 
-object test{
+import scala.collection.mutable.Stack
+
+object test extends App {
     println("testing...");
-    val user_file = new File("users.csv");
-    val user_source = Source.fromFile(user_file);
-
-    val check = user_source.mkString.split("\r");
-    var old_writer = new FileWriter(user_file, false);
-    old_writer.close();
-    old_writer = new FileWriter(user_file, true);
-
-    var count = 0;
+    val apiKey = 40130162;
+    val url = "https://www.thesportsdb.com/api/v1/json/40130162/lookupeventstats.php?id=1032723";
+    val result = scala.io.Source.fromURL(url).mkString;
     
-    for(line <- check) {
-        count = count + 1;
-        var out = count.toString + line;
-        old_writer.write(out);
+    var N = 0;
+
+    // var theStack = Stack[Char]();
+    for(i <- 0 until result.length) {
+        if(result(i) == '{')
+            N = N + 1;
     }
-    
-  
-    user_source.close();
-    old_writer.close();
+
+    println(N)
+    println(result)
+
+    var A = new Array[String](N);
+    for(i <- 0 until A.length)
+        A(i) = "";
+    var count = -1;
+
+    for(i <- 0 until result.length) {
+        if(result(i) == '{')
+            count = count + 1;
+        else if( (result(i) != '{' || result(i) != '}') && count > 0   )
+            A(count) = A(count) + result(i).toString
+        else if (result(i) == '}')
+            count = count - 1;
+        
+        //println("Count: " + count.toString)
+    } 
+    val dQ = '"';
+    var count2 = 0;
+    var B = new Array[String](50);
+    var unitedS = "";
+
+    for(line <- A) {
+        unitedS = unitedS + line + "/";
+    }
+    println(unitedS)
+    B = unitedS.replace(dQ.toString, "").replace("}", "").split("/");
+    println
+    B.foreach(println)
+
+        
 }
