cb9294d7172bc4ae65d42c35742d2c1c437a15c7
eperez176 <emanuel.perez@revature.net>
2021-11-02 22:33:48 -0500

    added function to scrub API data to csv format

--- src/main/scala/example/test.scala
+++ src/main/scala/example/test.scala
@@ -12,7 +12,7 @@ import scala.collection.mutable.Stack
 object test extends App {
     println("testing...");
     val apiKey = 40130162;
-    val url = "https://www.thesportsdb.com/api/v1/json/40130162/lookupeventstats.php?id=1032723";
+    val url = "https://www.thesportsdb.com/api/v1/json/1/eventspastleague.php?id=4328";
     val result = scala.io.Source.fromURL(url).mkString;
     
     var N = 0;
@@ -23,13 +23,12 @@ object test extends App {
             N = N + 1;
     }
 
-    println(N)
-    println(result)
-
     var A = new Array[String](N);
     for(i <- 0 until A.length)
         A(i) = "";
     var count = -1;
+
+    println("The raw input: \n" + result);
 
     for(i <- 0 until result.length) {
         if(result(i) == '{')
@@ -38,21 +37,98 @@ object test extends App {
             A(count) = A(count) + result(i).toString
         else if (result(i) == '}')
             count = count - 1;
-        
-        //println("Count: " + count.toString)
     } 
     val dQ = '"';
-    var count2 = 0;
-    var B = new Array[String](50);
+    var B = new Array[String](A.length);
     var unitedS = "";
+    var endOF = 0;
+    var backL = '/';
+    var fowardL = (92).toChar
 
+    println("\nA Size: " + A.size.toString + "\n")
     for(line <- A) {
-        unitedS = unitedS + line + "/";
-    }
-    println(unitedS)
-    B = unitedS.replace(dQ.toString, "").replace("}", "").split("/");
-    println
-    B.foreach(println)
+        endOF = endOF+1;
+        if(!line.isEmpty() && A.size > 2 && endOF != A.size) {
+            unitedS = unitedS + line + "#";
+        }
+        else if(!line.isEmpty())
+            unitedS = unitedS + line;
+    }   
+    println("\nThe Semi Parsed Input: \n" + unitedS);
 
-        
+    if(A.size > 2)
+        B = unitedS.replace(dQ.toString, "").replace("}", "").replace("]", "").replace("https:" + fowardL + backL + fowardL + backL, "").replace(fowardL.toString, "").replace(", ", ". ").split("#");
+        //println(unitedS.replace(dQ.toString, "").replace("}", "").replace("]", ""))
+    else
+        B(1) = unitedS.replace(dQ.toString, "").replace("}", "").replace("]", "").replace("https:" + fowardL + backL + fowardL + backL, "").replace(fowardL.toString, "").replace(", ", ". ");
+
+    println("\nSecond Parsed Data: ")
+    B.foreach(println)
+    println
+    var C = Array.ofDim[String](B.length, B(1).split(",").length)
+    if(A.size > 2) {
+        for(i <- 0 until B.length) { // Array having of record into several fields
+            C(i) = B(i).split(",")
+        }
+    }
+    else {
+        C(1) = B(1).split(",")
+    }
+    var D = Array.ofDim[String](2, C(1).length)
+    var out = "";
+    var out2 = "";
+
+    println("\nPARSED DATA:")
+
+    //println("\n" + unitedS.replace("https:" + fowardL + backL + fowardL + backL, ""))
+    if(A.size > 2) {
+        for(j <- 0 until C.length) {
+            if(C(j).length != 1) { // The empty data strings removed
+                for(i <- 0 until C(j).length) {
+                    if(C(j)(i).count(c => c == ':') > 1) {
+                        C(j)(i) = C(j)(i).replace(":", "#");
+                        C(j)(i) = C(j)(i).replaceFirst("#", ":");
+                    }
+                        
+                    D(0)(i) = C(j)(i).split(":").head;
+                    try {
+                        D(1)(i) = (C(j)(i).split(":"))(1)
+                    }
+                    catch {
+                        case _: Throwable => D(1)(i) = "null"
+                    }
+                }
+                for(j <- 0 until D(1).length)
+                    D(1)(j) = D(1)(j).replace("#",":")
+                
+                out = D(1).reduceLeft(_ + ","+ _);
+                out2 = D(0).reduceLeft(_ + "," + _);
+                println(out2)
+                println(out)
+                println
+            }
+        }
+    }
+    else {
+        for(i <- 0 until C(1).length) {
+            D(0)(i) = C(1)(i).split(":").head;
+            if(C(1)(i).count(c => c == ':') > 1) {
+                C(1)(i) = C(1)(i).replace(":", "#");
+                C(1)(i) = C(1)(i).replaceFirst("#", ":");
+            }
+            try {
+                D(1)(i) = (C(1)(i).split(":"))(1)
+            }
+            catch {
+                case _: Throwable => D(1)(i) = "null"
+            }
+        }
+        for(j <- 0 until D(1).length)
+            D(1)(j) = D(1)(j).replace("#",":")
+        out = D(1).reduceLeft(_ + ","+ _);
+        out2 = D(0).reduceLeft(_ + "," + _);
+        println(out2)
+        println(out)
+        println
+    }
 }
